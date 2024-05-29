bbf2ab50c77ca3b23d10ea8af483fb8f064a0f99
Matheus <matheushellf@gmail.com>
2024-05-29 00:22:40 -0300

    criando ListaLojaActivity, ListaProdutosAdapter, e layouts de produto_item.xml, activity_lista_loja.xml

--- app/src/main/java/com/mobile/ui/activity/ListaLojaActivity.kt
+++ app/src/main/java/com/mobile/ui/activity/ListaLojaActivity.kt
@@ -3,7 +3,9 @@ package com.mobile.ui.activity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.mobile.R
+import com.mobile.ui.adapter.ListaProdutosAdapter
 
 class ListaLojaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,16 @@ class ListaLojaActivity : AppCompatActivity() {
         setTitle("Loja")
         setContentView(R.layout.activity_lista_loja)
 
+        val nome = findViewById<TextView>(R.id.lista_nome_item)
+        nome.text = "Cesta de Frutas"
 
+        val desc = findViewById<TextView>(R.id.lista_descricao)
+        desc.text = "laranja, manga e maça"
 
+        val valor = findViewById<TextView>(R.id.lista_valor)
+        valor.text = "19.99"
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerview.adapter = ListaProdutosAdapter()
     }
 }
