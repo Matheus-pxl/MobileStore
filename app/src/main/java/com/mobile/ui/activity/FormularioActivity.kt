a890e988787f10f646938e7b38f62435407015cc
Matheus <matheushellf@gmail.com>
2024-05-28 22:16:00 -0300

    Initial commit

---
+++ app/src/main/java/com/mobile/ui/activity/FormularioActivity.kt
@@ -0,0 +1,11 @@
+package com.mobile.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle("Formulario de registro!")
+    }
+}
---
+++ app/src/main/java/com/mobile/ui/activity/ListaLojaActivity.kt
@@ -0,0 +1,17 @@
+package com.mobile.ui.activity
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.mobile.R
+
+class ListaLojaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTitle("Loja")
+        setContentView(R.layout.activity_lista_loja)
+
+
+
+    }
+}
