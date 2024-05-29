f48efb1fa6ef2a63c224df765a1b8d38017bf86f
Matheus <matheushellf@gmail.com>
2024-05-29 10:04:23 -0300

    bind no FormularioActivity.kt, criação do layout activity_formulario.xml

--- app/src/main/java/com/mobile/ui/activity/FormularioActivity.kt
+++ app/src/main/java/com/mobile/ui/activity/FormularioActivity.kt
@@ -1,11 +1,43 @@
 package com.mobile.ui.activity
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.mobile.R
+import com.mobile.model.Produto
+import java.math.BigDecimal
 
-class FormularioActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setTitle("Formulario de registro!")
+class FormularioActivity : AppCompatActivity(R.layout.activity_formulario) {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        val botaoSalvar = findViewById<Button>(R.id.form_botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.form_nome_produto)
+            val nome = campoNome.text.toString()
+
+            val campoDescricao = findViewById<EditText>(R.id.form_descricao_produto)
+            val descricao = campoDescricao.text.toString()
+
+            val campoValor = findViewById<EditText>(R.id.form_valor_produto)
+            val valorEmText = campoValor.text.toString()
+            val valor = if(valorEmText.isBlank()){
+                BigDecimal.ZERO
+            }else{
+                BigDecimal(valorEmText)
+            }
+            Produto(
+                nome=nome,
+                descricao=descricao,
+                valor= valor
+            )
+        }
+
     }
+
 }
--- app/src/main/java/com/mobile/ui/activity/ListaLojaActivity.kt
+++ app/src/main/java/com/mobile/ui/activity/ListaLojaActivity.kt
@@ -1,28 +1,37 @@
 package com.mobile.ui.activity
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobile.R
+import com.mobile.model.Produto
 import com.mobile.ui.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
-class ListaLojaActivity : AppCompatActivity() {
+class ListaLojaActivity : AppCompatActivity(R.layout.activity_lista_loja) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTitle("Loja")
-        setContentView(R.layout.activity_lista_loja)
 
-        val nome = findViewById<TextView>(R.id.lista_nome_item)
-        nome.text = "Cesta de Frutas"
+//        val nome = findViewById<TextView>(R.id.lista_nome_item)
+//        nome.text = "Cesta de Frutas"
+//
+//        val desc = findViewById<TextView>(R.id.lista_descricao)
+//        desc.text = "laranja, manga e maça"
+//
+//        val valor = findViewById<TextView>(R.id.lista_valor)
+//        valor.text = "19.99"
 
-        val desc = findViewById<TextView>(R.id.lista_descricao)
-        desc.text = "laranja, manga e maça"
-
-        val valor = findViewById<TextView>(R.id.lista_valor)
-        valor.text = "19.99"
-
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerview.adapter = ListaProdutosAdapter()
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(
+                nome = "teste1", descricao = "descricao do item 1", valor = BigDecimal("19.99")
+            ),
+            Produto(
+                nome = "teste2", descricao = "descricao do item 2", valor = BigDecimal("29.99"),
+            )
+        ))
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
