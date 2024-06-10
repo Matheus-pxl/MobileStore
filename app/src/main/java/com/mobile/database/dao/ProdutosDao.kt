fd3d2e2bb288da256712bb50e615e9c87442262a
Matheus <matheushellf@gmail.com>
2024-06-10 15:11:18 -0300

    personalizando card do produto_item.xml

--- app/src/main/java/com/mobile/database/dao/ProdutosDao.kt
+++ app/src/main/java/com/mobile/database/dao/ProdutosDao.kt
@@ -1,16 +1,26 @@
 package com.mobile.database.dao
 
 import com.mobile.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         produtos.add(produto)
     }
-    fun buscaTodos():List<Produto>{
+
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+
+            //exemplo para nao ter q  adicionar um item toda hora
+            Produto(
+                nome = "salada de frutas",
+                descricao = "laranja,maça, pera e uva",
+                valor = BigDecimal("19.55")
+            )
+        )
     }
 }
